01a7d05b5cf453394a72b8492cc50d85c567e843
Dionicio Diaz <dionicio007@hotmail.com>
2020-02-23 02:07:58 -0400

    import home and auth pages to the main router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    loadChildren: () => import('@pages/auth/auth.module').then(m => m.AuthPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('@pages/home/home.module').then(m => m.HomePageModule)
+  },
 ];
 @NgModule({
   imports: [
@@ -13,4 +17,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
