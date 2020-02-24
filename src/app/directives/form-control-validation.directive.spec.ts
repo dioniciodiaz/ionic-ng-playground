be04ecbf8b99e5e440200d091e1c5fc9fc1b0645
Dionicio Diaz <dionicio007@hotmail.com>
2020-02-24 18:33:20 -0400

    add form control validation directive and service

---
+++ src/app/directives/form-control-validation.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FormControlValidationDirective } from './form-control-validation.directive';
+
+describe('FormControlValidationDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FormControlValidationDirective();
+    expect(directive).toBeTruthy();
+  });
+});
