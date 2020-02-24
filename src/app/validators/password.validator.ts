c665a845a0efd16321b34001c4cf1693081cff00
Dionicio Diaz <dionicio007@hotmail.com>
2020-02-24 18:58:28 -0400

    fix password validator

--- src/app/validators/password.validator.ts
+++ src/app/validators/password.validator.ts
@@ -3,7 +3,7 @@ import { AbstractControl } from '@angular/forms';
 export function ValidatePassword(control: AbstractControl) {
     const value = control.value
     const regex = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$';
-    if (value.match(regex).length > 0) {
+    if (value && value.length > 6 && !value.match(regex)) {
         return { passwordField: true };
     }
     return null;
