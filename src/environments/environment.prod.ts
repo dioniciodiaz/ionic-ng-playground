3dba47e0707b4129ba5d8ec3750b8ff655a42059
Dionicio Diaz <dionicio007@hotmail.com>
2020-02-25 00:34:20 -0400

    move parse server url to environments files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   parseAplicationId: 'K8NVRkFVIJWTzBBLyJn13G9CN0TKOsq4BlyHiOYR',
-  parseJavaScriptKey: '0FgrlpslgKpBeZgN5V71PimdTOLAhpAIbwWxxbKJ'
+  parseJavaScriptKey: '0FgrlpslgKpBeZgN5V71PimdTOLAhpAIbwWxxbKJ',
+  parseServerURL: 'https://parseapi.back4app.com'
 };
