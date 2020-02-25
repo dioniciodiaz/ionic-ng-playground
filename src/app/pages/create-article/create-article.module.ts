ad0518e400d5ee6b5b10034926dc802c331506b5
Dionicio Diaz <dionicio007@hotmail.com>
2020-02-25 05:12:38 -0400

    finish create article behaviour

--- src/app/pages/create-article/create-article.module.ts
+++ src/app/pages/create-article/create-article.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -11,10 +11,10 @@ import { CreateArticlePage } from './create-article.page';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     CreateArticlePageRoutingModule
   ],
   declarations: [CreateArticlePage]
 })
-export class CreateArticlePageModule {}
+export class CreateArticlePageModule { }
--- src/app/pages/create-article/create-article.page.ts
+++ src/app/pages/create-article/create-article.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { UserService } from "@services/user.service";
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-article',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-article.page.scss'],
 })
 export class CreateArticlePage implements OnInit {
+  articleForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private createArticleFormBuilder: FormBuilder,
+    private userService: UserService,
+    public toastCtrl: ToastController,
+    public router: Router) { }
 
   ngOnInit() {
+
+    this.articleForm = this.createArticleFormBuilder.group({
+      content: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+    });
   }
 
+  get content() {
+    return this.articleForm.get('content');
+  }
+
+  createArticle() {
+    const { content } = this.articleForm.value;
+    this.userService.createArticle(content)
+      .then(() => {
+        this.showToast(`Article added Successfully`);
+        this.articleForm.reset()
+        this.router.navigateByUrl('/home/article-list') //navigateByUrl('/home');
+      }).catch(async (err: HttpErrorResponse) => this.showToast(err.message));
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
 }
