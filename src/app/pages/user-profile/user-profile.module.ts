6eb900ffee27699231dae9887132d59440d1aaf5
Dionicio Diaz <dionicio007@hotmail.com>
2020-02-25 04:29:05 -0400

    finish user profile behaviour

--- src/app/pages/user-profile/user-profile.module.ts
+++ src/app/pages/user-profile/user-profile.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { UserProfilePageRoutingModule } from './user-profile-routing.module';
@@ -11,10 +10,10 @@ import { UserProfilePage } from './user-profile.page';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    UserProfilePageRoutingModule
+    UserProfilePageRoutingModule,
   ],
   declarations: [UserProfilePage]
 })
-export class UserProfilePageModule {}
+export class UserProfilePageModule { }
